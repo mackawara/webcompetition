3de88e2d051f5ad2a9035408075796d62a3f0088
mackawara <84657473+mackawara@users.noreply.github.com>
2022-05-19 14:51:24 +0200

    added register route and validation to route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 //const { dirname } = require("path/posix")
 const app = express();
 const PORT = process.env.PORT || 3000;
-const multer=require("multer")
+const multer = require("multer");
 const upload = multer();
 
 // mongoose configuration
@@ -19,17 +19,17 @@ const options = {
   useUnifiedTopology: true,
   dbname: databaseName,
 };
-const uri = process.env.MONGODB_URI/* process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOLHQ_URI  */
+const uri =
+  process.env
+    .MONGODB_URI; /* process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOLHQ_URI  */
 
-
-let connection = mongoose.connect(uri || process.env.DB_URI, options );
+let connection = mongoose.connect(uri || process.env.DB_URI, options);
 
 const database = mongoose.connection;
 database.on("error", console.error.bind(console, "connection error:"));
 database.once("open", function () {
   console.log(`DAtabase connection established on this uri :${uri}`);
-
-}); 
+});
 app.listen(PORT, () => {
   console.log(`Server started, listening on  port: ${PORT} `);
 });
@@ -40,13 +40,22 @@ app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
 app.use(upload.array());
+/* MiddleWare */
+const {
+  userValidationRules,
+  validateEntrant,
+} = require("./middleware/validation.js");
+const saveEntrantToDataBase=require("./middleware/saveToDB.js")
 //ROUTES
-app.post("./register", (req,res)=>{
-    
-})
+app.post(
+  "/register",
+   userValidationRules(),validateEntrant,saveEntrantToDataBase, (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+  }
+);
 //HOME PAGE
 
- 
 // BOOKING PAGE
 //const { body, validationResult } = require("express-validator");
 
