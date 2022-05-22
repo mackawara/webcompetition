ab5ea8a7e77730c3d307679ab34529870a7a56ce
mackawara <84657473+mackawara@users.noreply.github.com>
2022-05-22 14:59:10 +0200

    removed regex testing

--- public/index.js
+++ public/index.js
@@ -6,37 +6,29 @@ window.addEventListener("DOMContentLoaded", async () => {
 
     formValidation();
   });
+  function reset(){
+    
+  }
   /* register(); */
   async function formValidation() {
-    let formData = new FormData();
     /* input fields */
     const fullName = document.getElementById("fullName");
     const email = document.getElementById("email");
     const bizLocation = document.getElementById("bizLocation");
-    const bizdescription = document.getElementById("bizDescription");
+    const bizDescription = document.getElementById("bizDescription");
     const phoneNumber = document.getElementById("phoneNumber");
-    const fblink = document.getElementById("fblink");
+    const fbLink = document.getElementById("fbLink");
 
     let inputErrors = [];
 
     function inputValidation(inputsField) {
-      const entityMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': "&quot;",
-        "'": "&#39;",
-        "/": "&#x2F;",
-        "`": "&#x60;",
-        "=": "&#x3D;",
-      };
-
       this.inputsField = inputsField;
       const small = inputsField.parentElement.querySelector("small");
       const value = this.inputsField.value;
       /* set error is the function which catches all errors and inputs the errors CSS */
       this.setError = function () {
         inputsField.parentElement.className = "input-group error";
+        inputErrors.push(inputsField.name);
       };
       /* function which checks if field is empty */
       this.isEmpty = function () {
@@ -51,12 +43,12 @@ window.addEventListener("DOMContentLoaded", async () => {
         /* returns true if value is tool long */
         return value.length > figure;
       };
-      this.regexText = function (regex) {
-        return !regex.test(value);
+      this.regexTest = function (regex) {
+        return regex.test(value);
       };
       /* function to run when the field is empty */
       this.empty = function () {
-        console.log(`${inputsField.name} is emtpy`);
+        console.log(`${inputsField.name} is empty`);
         small.innerText = inputsField.name + " cannot be empty";
         inputErrors.push(inputsField.name);
       };
@@ -67,33 +59,28 @@ window.addEventListener("DOMContentLoaded", async () => {
 
         inputErrors.push(`${inputsField.name}`);
       };
-      this.sanitise = (string) => {
-        return String(string).replace(/[&<>"'`=\/]/g, function (s) {
-          return entityMap[s];
-        });
-      };
       this.setSuccess = async function () {
-        console.log(` ${inputsField.name} was recorded successfully`);
-        const sanitisedValue = this.sanitise(value);
-        console.log(sanitisedValue);
-        //small.innerText = message;
-        inputsField.parentElement.className = "input-group success";
-        await formData.append(`${this.inputsField.name}`, `${sanitisedValue}`);
+        /* first remove existing enty in inout errors , if any */
+        const index = inputErrors.indexOf(inputsField.name);
+        if (index > -1) {
+          inputErrors.splice(index, 1); // 2nd parameter means remove one item only
+        }
       };
+      /* add styling to show successful validation */
+      inputsField.parentElement.className = "input-group success";
+      
     }
 
     /* Name Validation */
 
-    /* Email validation */
     const fullNamefield = new inputValidation(fullName);
+    /* check if field is empty */
     if (fullNamefield.isEmpty()) {
       fullNamefield.empty(), fullNamefield.setError();
     } else if (
+      /* check name  if valid input */
       fullNamefield.tooShort(4) ||
-      fullNamefield.tooLong(65) ||
-      fullNamefield.regexText(
-        /(?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$)/
-      )
+      fullNamefield.tooLong(65)
     ) {
       fullNamefield.invalid();
       fullNamefield.setError();
@@ -105,10 +92,10 @@ window.addEventListener("DOMContentLoaded", async () => {
     if (emailfield.isEmpty()) {
       emailfield.empty(), emailfield.setError();
     } else if (
-      emailfield.tooShort(4) ||
-      emailfield.regexText(
+      emailfield.tooShort(4) /*  ||
+      emailfield.regexTest(
         /(?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$)/
-      )
+      ) */
     ) {
       emailfield.invalid();
       emailfield.setError();
@@ -121,32 +108,38 @@ window.addEventListener("DOMContentLoaded", async () => {
     if (phoneNumberField.isEmpty()) {
       phoneNumberField.empty(), phoneNumberField.setError();
     } else if (
+      /* check phone number length */
       phoneNumberField.tooShort(10) ||
       phoneNumberField.tooLong(14) ||
-      phoneNumberField.regexText(/(\+263|0)7[7-8|1|3][0-9]{7}$/)
+      /* check if valid Zimbabwean number */
+      phoneNumberField.regexTest(/(\+263|0)7[7-8|1|3][0-9]{7}$/)
     ) {
       phoneNumberField.setError(), phoneNumberField.invalid();
     } else {
       phoneNumberField.setSuccess;
     }
 
-    const fblinkField = new inputValidation(fblink);
-    if (fblinkField.tooShort(3)) {
-      fblinkField.setError();
-      fblinkField.invalid();
+    const fbLinkField = new inputValidation(fbLink);
+    if (fbLinkField.isEmpty()) {
+      fbLinkField.empty();
+      fbLinkField.setError();
+    } else if (fbLinkField.tooShort(3)) {
+      fbLinkField.setError();
+      fbLinkField.invalid();
     } else {
-      fblinkField.setSuccess();
+      fbLinkField.setSuccess();
     }
-    const bizdescriptionField = new inputValidation(bizdescription);
-    if (bizdescriptionField.isEmpty()) {
-      bizdescriptionField.empty();
+    const bizDescriptionField = new inputValidation(bizDescription);
+    if (bizDescriptionField.isEmpty()) {
+      bizDescriptionField.setError();
+      bizDescriptionField.empty();
     } else if (
-      bizdescriptionField.tooShort(200) ||
-      bizdescriptionField.tooLong(800)
+      bizDescriptionField.tooShort(200) ||
+      bizDescriptionField.tooLong(800)
     ) {
-      bizdescriptionField.setError(), bizdescriptionField.invalid();
+      bizDescriptionField.invalid(), bizDescriptionField.setError();
     } else {
-      bizLocationField.setSuccess();
+      bizDescriptionField.setSuccess();
     }
     const bizLocationField = new inputValidation(bizLocation);
     if (bizLocationField.isEmpty()) {
@@ -156,66 +149,69 @@ window.addEventListener("DOMContentLoaded", async () => {
     } else {
       bizLocationField.setSuccess();
     }
+
+    if (inputErrors.length == 0) {
+      console.log(inputErrors);
+      sendRegistrationForm();
+    }
   }
 
-  /*  Sends the form Data to the back end 
-      async function sendRegistrationForm() {
-        const options = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          mode: "cors",
-          redirect: "follow",
-          body: JSON.stringify(formData),
-        };
-        fetch("/registration", options).then((response) =>
-          response.json().then((data) => {
-            const result = document.getElementById("results");
-            result.style.display = "block";
-    
-            if (response.status == "200") {
-              form.remove();
-              document.getElementById(
-                "confirmation" 
-              ).innerText = ` Thank you ${data.fullName} for registering.Your details have been succssfully recorded as follows!!`;
-              console.log("SUCCESS");
-              for (const any in data) {
+  /*  Sends the form Data to the back end  */
+  async function sendRegistrationForm() {
+    let formData = new FormData(form);
+    console.log(formData);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      mode: "cors",
+      redirect: "follow",
+      body: formData,
+    };
+    const response = await fetch("/registration", options);
+    const data = await response.json();
+    const result = document.getElementById("results");
+    result.style.display = "block";
+
+    if (response.status == "200") {
+      form.remove();
+      document.getElementById(
+        "confirmation"
+      ).innerText = ` Thank you ${data.fullName} for participating, you will hear from our team soon!`;
+      console.log("SUCCESS");
+      /* for (const any in data) {
                 const p = document.createElement("p");
                 const personalDetails = `${any}: ${data[any]}`;
                 p.innerText = personalDetails;
                 result.appendChild(p);
                 result.classList.remove("error");
-              }
-            } else if (response.status == "500") {
-              console.log("Error");
-              const regConfirmation = document.getElementById(
-                "registrationConfirmation"
-              );
-              regConfirmation.innerText = data.response;
-              regConfirmation.parentElement.classList.add("success");
-            } else if (response.status == "422") {
-              console.log("Error");
-              document.getElementById(
-                "registrationConfirmation"
-              ).innerText = ` Your submission contains errors!!`;
-              for (const any in data) {
-                result.classList.add("error");
-                const p = document.createElement("p");
-                const personalDetails = ` ${data[any].properties.message}`;
-                p.innerText = personalDetails;
-                document
-                  .getElementById(`${any}`)
-                  .parentElement.classList.remove("success");
-                document
-                  .getElementById(`${any}`)
-                  .parentElement.classList.add("error");
-    
-                console.log(`${any}`);
-                result.appendChild(p);
-              }
-            }
-          })
-        );
-      } */
+              } */
+    } else if (response.status == "409") {
+      console.log(
+        "Error duplicate entry. Please check if you havent already made a submission"
+      );
+      const confirmation = document.getElementById("registrationConfirmation");
+      confirmation.innerText = data.response;
+      confirmation.parentElement.classList.add("error");
+    } else if (response.status == "422") {
+      console.log("Error");
+      document.getElementById(
+        "registrationConfirmation"
+      ).innerText = ` Your submission contains errors!!`;
+      for (const any in data) {
+        result.classList.add("error");
+        const p = document.createElement("p");
+        const personalDetails = ` ${data[any].properties.message}`;
+        p.innerText = personalDetails;
+        document
+          .getElementById(`${any}`)
+          .parentElement.classList.remove("success");
+        document.getElementById(`${any}`).parentElement.classList.add("error");
+
+        console.log(`${any}`);
+        result.appendChild(p);
+      }
+    }
+  }
 });
